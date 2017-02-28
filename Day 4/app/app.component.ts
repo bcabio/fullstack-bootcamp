8ec7ef70b7faf4cda55a59c853ef3ebe4c5320b4
Nishant Sinha <nishnha@gmail.com>
2017-02-28 10:11:59 -0500

    Created storeProject branch for starting day 2

--- Day 4/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { ItemComponent } from './item.component';
-@Component({
-    selector: 'my-app',
-    template: ` <item> </item>
-    			`
-})
-export class AppComponent { }
